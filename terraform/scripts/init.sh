2a64cc7493f7446f20745f48cc0fdc273fb6dd05
sashaz <6964653+OZahnitko@users.noreply.github.com>
2021-12-07 15:18:50 +1000

    right img

--- terraform/scripts/init.sh
+++ terraform/scripts/init.sh
@@ -3,5 +3,4 @@ sudo yum update -y
 sudo amazon-linux-extras install docker -y
 sudo service docker start
 sudo usermod -a -G docker ec2-user
-sudo docker run -d -p 80:80 ozahnitko/ting-client:latest
-sudo docker run -d -p 5000:5000 -e PORT=5000 ozahnitko/ting-api:latest
+sudo docker run -d -p 80:80 ozahnitko/tks-exercise-tracker
